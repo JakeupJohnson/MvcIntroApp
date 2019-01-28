14f1e0cdb5a4ceae1c6e94cc68da766cd4609c8c
JakeupJohnson <46660526+JakeupJohnson@users.noreply.github.com>
2019-01-27 20:12:29 -0600

    Add files via upload

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,29 @@
+﻿using MvcIntroApp.Models;
+using MvcIntroApp.Shared.Enums;
+using MvcIntroApp.Shared.Orchestrators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcIntroApp.Controllers
+{
+    public class CourseController : Controller
+    {
+        // GET: Course
+        public ActionResult Index()
+        {
+            var courseOrchestrator = new CourseOrchestrator();
+
+            var model = new SemesterModel
+            {
+                Year = 2019,
+                Semester = SemesterEnum.Spring,
+                Courses = courseOrchestrator.GetAllCourses()
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ Models/SemesterModel.cs
@@ -0,0 +1,17 @@
+﻿using MvcIntroApp.Shared.Enums;
+using MvcIntroApp.Shared.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcIntroApp.Models
+{
+    public class SemesterModel
+    {
+        public int Year { get; set; }
+        public SemesterEnum Semester { get; set; }
+        public string SemesterName => Semester.ToString();
+        public List<CourseViewModel> Courses { get; set; }
+    }
+}
